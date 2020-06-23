82e1b1f622eeda6d21b97a446c3ee4efa91ae003
Shota Murano <dcmsd09072241242@gmail.com>
2020-06-23 15:18:27 +0900

    add getBorrowNumのテスト

--- src/redhot/dao/MainDAOTest.java
+++ src/redhot/dao/MainDAOTest.java
@@ -1,26 +1,75 @@
 package redhot.dao;
 
+import static org.junit.Assert.*;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MainDAOTest extends MainDAO {
 
-	@Test
-	void test() {
-		MainDAO dao = new MainDAO();
-		Assertions.assertEquals(111111, dao.getBorrowNum(111111));
-	}
+	//	@Test
+	//	@DisplayName("Dataベースに接続できるかの疎通テスト")
+	//	void test() {
+	//		MainDAO dao = new MainDAO();
+	//		try {
+	//			Assertions.assertEquals(1, dao.getSqlResult(111111));
+	//		} catch (DAOException e) {
+	//			fail();
+	//			e.printStackTrace();
+	//		}
+	//		return;
+	//	}
 
-	@Test
-	void test1() {
-		MainDAO dao = new MainDAO();
-		Assertions.assertEquals(true, dao.isLate());
-	}
+	//	@Test
+	//	void test1() {
+	//		MainDAO dao = new MainDAO();
+	//		Assertions.assertEquals(true, dao.isLate());
+	//	}
 
 	@Test
 	void test2() {
 		MainDAO dao = new MainDAO();
 		Assertions.assertEquals("hello", dao.getBookStatus(111));
+	}
+
+	@Test
+	@DisplayName("貸出数を取得できるかの確認（ユーザID 1番の人）")
+	void test3() {
+		MainDAO dao = new MainDAO();
+		try {
+			Assertions.assertEquals(3, dao.getBorrowNum(1));
+		} catch (DAOException e) {
+			fail();
+			e.printStackTrace();
+		}
+		return;
+	}
+
+	@Test
+	@DisplayName("貸出数を取得できるかの確認（ユーザIDがない人）")
+	void test4() {
+		MainDAO dao = new MainDAO();
+		try {
+			Assertions.assertEquals(0, dao.getBorrowNum(2));
+		} catch (DAOException e) {
+			fail();
+			e.printStackTrace();
+		}
+		return;
+	}
+
+	@Test
+	@DisplayName("貸出数を取得できるかの確認（ユーザIDがマイナスの値の人）")
+	void test5() {
+		MainDAO dao = new MainDAO();
+		try {
+			Assertions.assertEquals(0, dao.getBorrowNum(-1));
+		} catch (DAOException e) {
+			fail();
+			e.printStackTrace();
+		}
+		return;
 	}
 
 }
