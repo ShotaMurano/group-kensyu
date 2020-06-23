60151fdd0253cb9d8f0bc9edadfb0fd7a51a039e
Shota Murano <dcmsd09072241242@gmail.com>
2020-06-23 15:18:01 +0900

    add getBorrowNum（ユーザの借りている本の数を返す）

--- src/redhot/dao/MainDAO.java
+++ src/redhot/dao/MainDAO.java
@@ -2,6 +2,9 @@ package redhot.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MainDAO {
 
@@ -25,17 +28,48 @@ public class MainDAO {
 		}
 	}
 
-	public int getBorrowNum(int id) {
-		this.id = id;
-		return this.id;
+	//テスト用メソッド
+	//	public int getSqlResult(int id) throws DAOException {
+	//		String sql = "SELECT * FROM users";
+	//		try (Connection con = getConnection();
+	//				PreparedStatement st = con.prepareStatement(sql);) {
+	//			ResultSet rs = st.executeQuery();
+	//			rs.next();
+	//			System.out.println(rs.getInt("id"));
+	//			return rs.getInt("id");
+	//		} catch (SQLException e) {
+	//			throw new DAOException("レコードの取得に失敗しました", e);
+	//		}
+	//
+	//	}
+
+	//ユーザIDを入れて、借りてる本の数を返す
+	public int getBorrowNum(int id) throws DAOException {
+		String sql = "SELECT * FROM borrow where user_id=?";
+		int count = 0;
+		try (Connection con = getConnection();
+				PreparedStatement st = con.prepareStatement(sql);) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				count++;
+			}
+			return count;
+		} catch (SQLException e) {
+			throw new DAOException("レコードの取得に失敗しました", e);
+		}
+
 	}
 
+	//ユーザIDを入れて、予約している本の数を返す
 	public int getPreorderNum(int id) {
+
 		this.id = id;
 		return this.id;
 	}
 
-	public boolean isLate() {
+	//ユーザの延滞があるかを
+	public boolean isLate(int id) {
 		return true;
 	}
 
