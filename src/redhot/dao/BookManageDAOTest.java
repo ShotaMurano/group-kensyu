e293d0891775ffd0f57e768965b2882196cedc77
Takaki Kawamura <docosys.r2kawamura@gmail.com>
2020-06-24 14:41:28 +0900

    PreorderDAO test & bookmanager All word search commit

--- src/redhot/dao/BookManageDAOTest.java
+++ src/redhot/dao/BookManageDAOTest.java
@@ -4,13 +4,14 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
-class BookManageDAOTest {
+public class BookManageDAOTest {
 
 	@Test
 	@DisplayName("ISBN番号での検索")
-	void test() throws DAOException {
+	public void test() throws DAOException {
 		BookManageDAO dao = new BookManageDAO();
-		Assertions.assertEquals(null, dao.searchBook("9788281642674"));
+		Assertions.assertEquals("9788281642675",
+				dao.searchBook("9788281642675", "", "9", "", "新潮文庫", "").get(0).getBookIsbn());
 	}
 
 }
